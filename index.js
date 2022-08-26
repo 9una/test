64383dd8ea17273eb9ec5826d9f9e2908ac7c72f
guna_9 <89364607+9una@users.noreply.github.com>
2022-08-26 16:21:56 +0900

    제발

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ function navCall(event){
     const mobileGNB = document.querySelector('.m_gnb');
     mobileGNB.classList.toggle('active');
     event.target.classList.toggle('active');
-    if(mobileGNB.classList.contains('active')){
-        document.querySelector('html').style.overflowY = "hidden";
-    }
-    if(mobileGNB.classList.contains('active') == false){
-        document.querySelector('html').style = "";
-    }
+    document.querySelector('body').classList.toggle('non-scroll');
+    // if(mobileGNB.classList.contains('active')){
+    //     document.querySelector('html').style.overflow = "hidden";
+    // }
+    // if(mobileGNB.classList.contains('active') == false){
+    //     document.querySelector('html').style = "";
+    // }
     //document.querySelector('body').classList.toggle('non-scroll');
 }
