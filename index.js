a19838c2a6c4b48255eab9fd1acfdab67f616f90
guna_9 <89364607+9una@users.noreply.github.com>
2022-08-26 16:26:57 +0900

    제발22

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ function navCall(event){
     const mobileGNB = document.querySelector('.m_gnb');
     mobileGNB.classList.toggle('active');
     event.target.classList.toggle('active');
-    document.querySelector('body').classList.toggle('non-scroll');
-    // if(mobileGNB.classList.contains('active')){
-    //     document.querySelector('html').style.overflow = "hidden";
-    // }
-    // if(mobileGNB.classList.contains('active') == false){
-    //     document.querySelector('html').style = "";
-    // }
+    if(mobileGNB.classList.contains('active')){
+        document.querySelector('body').style.overflow = "hidden";
+    }
+    if(mobileGNB.classList.contains('active') == false){
+        document.querySelector('body').style = "";
+    }
     //document.querySelector('body').classList.toggle('non-scroll');
 }
