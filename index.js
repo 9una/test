8ca0b860df5cff64ed50a4078e0f7810d62ba278
guna_9 <89364607+9una@users.noreply.github.com>
2022-08-26 16:01:20 +0900

    js test

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+
+
 function navCall(event){
     const mobileGNB = document.querySelector('.m_gnb');
     mobileGNB.classList.toggle('active');
     event.target.classList.toggle('active');
-    document.querySelector('body').classList.toggle('non-scroll');
+    if(mobileGNB.classList.contains('active')){
+        document.querySelector('body').style.overflow = "hidden";
+    }
+    if(mobileGNB.classList.contains('active') == false){
+        document.querySelector('body').style = "";
+    }
+    //document.querySelector('body').classList.toggle('non-scroll');
 }
