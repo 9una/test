53941b41ba72363840692be5a6cc2e3b1f9de567
guna_9 <89364607+9una@users.noreply.github.com>
2022-08-26 16:17:52 +0900

    body > html

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ function navCall(event){
     mobileGNB.classList.toggle('active');
     event.target.classList.toggle('active');
     if(mobileGNB.classList.contains('active')){
-        document.querySelector('body').style.overflowY = "hidden";
         document.querySelector('html').style.overflowY = "hidden";
     }
     if(mobileGNB.classList.contains('active') == false){
-        document.querySelector('body').style = "";
+        document.querySelector('html').style = "";
     }
     //document.querySelector('body').classList.toggle('non-scroll');
 }
