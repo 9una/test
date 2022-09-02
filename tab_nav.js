e2b670e6cbc4c6dd070da77f5550011d477768f6
guna_9 <89364607+9una@users.noreply.github.com>
2022-09-02 17:56:02 +0900

    quickmenu code text
    0902

---
+++ tab_nav.js
@@ -0,0 +1,55 @@
+var App = {
+
+	accessibleMenu: function(){
+		$menu = $('.gnb');
+		$menuItem = $menu.find('> li > a');
+
+		$subMenu = $('.submenu');
+		$subMenuItem = $subMenu.find('> li > a');
+        $submenuLastItem = $subMenu.find('> li:last-child > a');
+
+		$menuItem.bind({
+			focus: function(){
+				$subMenu.removeClass('is-show');
+				if($(this).next($subMenu)){
+					$(this).next($subMenu).addClass('is-show');
+				}
+			},
+
+			blur: function(){
+				$subMenu.removeClass('is-show');
+			}
+        });
+
+		$subMenuItem.bind({
+			focus: function(){
+				$(this).parent().parent().addClass('is-show');
+			}
+        });
+
+		$submenuLastItem.bind({
+			blur: function(){
+				$subMenu.removeClass('is-show');
+			}
+        });
+    },
+	cancelAccessibleMenu: function(){
+		$(document).click(function(){
+			if($subMenu.hasClass('is-show')){
+				$subMenu.removeClass('is-show');
+            }
+        });
+	}
+
+};
+
+$(function(){
+    App.accessibleMenu();
+	App.cancelAccessibleMenu();
+});
+
+$(".btn-quick").bind({
+    click:function(){
+        $('.quick-wrap').toggleClass('active');
+    }
+})
