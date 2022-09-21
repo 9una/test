15645eb2b6d68e036927bb6414088d35d1183c8e
guna_9 <89364607+9una@users.noreply.github.com>
2022-09-21 16:46:21 +0900

    swiper in quick menu

---
+++ quick-tab-swiper/index.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    slidesPerGroup : 5, //그룹으로 묶어서 넘김
+    slidesPerView: 5, //동시에 보여줄 슬라이드 갯수
+    spaceBetween: 0,
+    freeMode: true,
+})
