455dc09d7565cf58b783384c57be89f0c6defa03
guna_9 <89364607+9una@users.noreply.github.com>
2022-10-13 17:11:39 +0900

    media query + text lenth limit
    반응형 사이즈에 따라 글자수 제한 말줄임 변경함

---
+++ limit text length/index.js
@@ -0,0 +1,36 @@
+// (반응형) : 윈도우 사이즈에 따른 말줄임 글자수 변경
+
+$('div p').each(function(){
+    var length;
+    const text = $(this).text(); //기존 텍스트 저장
+    
+    function lengthChange(){
+        if($(window).width() > 768){
+            length = 80;
+        } else {
+            length = 30;
+        }
+    }
+    lengthChange();
+    
+    $(this).each(function () {
+        if ($(this).text().length >= length) {
+            $(this).text($(this).text().substr(0, length) + "....!!");
+        }
+    });
+    
+    $(window).resize(function(){
+        lengthChange();
+
+        $('div p').each(function(){
+            $(this).text(text);
+
+            $(this).each(function () {
+                if ($(this).text().length >= length) {
+                    $(this).text($(this).text().substr(0, length) + "....!!");
+                }
+            });
+        
+        })
+    })
+})
